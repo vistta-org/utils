9d4eebe0aa194357691f022823aa47e27ce5bbef
Tiago Terenas Almeida <tiagoterenas@gmail.com>
2024-09-02 10:44:38 +0100

    #9

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-export * from "./lib/console.js";
 export * from "./lib/datetime.js";
-export * from "./lib/fs.js";
 export * from "./lib/generic.js";
 export * from "./lib/object.js";
 export * from "./lib/promise.js";
